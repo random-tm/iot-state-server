df544c4bf3c2f0d80d8a7a09048ba5ba90087f20
Lucas Smith <lucas@lucasesmith.com>
2022-09-18 22:38:35 -0400

    Backup state code.

--- src/routes/state/routes.js
+++ src/routes/state/routes.js
@@ -1,16 +1,19 @@
 import internal from "../../internal.js";
 import getAutomationServer from "../../net/getAutomationServer.js";
+import getStateBackup from "../../net/getStateBackup.js";
 import send from "../../net/send/index.js";
 import { getStateFiltered, updateState } from "../../state.js"
 
 export default (method, params, ctx) => {
     if(method === "POST"){
         const automationUrl = getAutomationServer();
+        const backupUrl = getStateBackup();
         const data = updateState(params);
         if(data){
             if(!data.error){
                 const safeAutomationState = internal(data);
                 send(automationUrl, {state: safeAutomationState});
+                send(backupUrl, {state: safeAutomationState});
             }
         }
         ctx.body = "OK"
