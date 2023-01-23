e3f4fd873bd37d3f7c650e1fbd687d609566c1da
Lucas Smith <lucas@lucasesmith.com>
2023-01-22 21:05:26 -0500

    Fix fatal error which can occur when the state system has to execute retries.

--- src/net/send/retry.js
+++ src/net/send/retry.js
@@ -2,7 +2,7 @@ export default (pendingRequest, delay) => {
     setTimeout(() => {
         axios.post(pendingRequest.url, pendingRequest.data).then(() => {
             const key = makeKey(pendingRequest.url, pendingRequest.data);
-            _.remove(pendingRequests, (request) => {
+            _.remove(pendingRequest, (request) => {
                 return request.key === key;
             })
         })
